fe45c1ab90c53caa3e1d1ed2c38f89b25f74a7de
n4r1tahhhh <ryutaro5126@fuji.waseda.jp>
2020-10-12 03:06:53 +0000

    change task schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201011100955) do
+ActiveRecord::Schema.define(version: 20201012025701) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tasks_on_name", unique: true
   end
 
 end
